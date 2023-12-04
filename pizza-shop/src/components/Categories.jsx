c3dada8c97f11f20fa1cfb3b2d486f5a1543c1f4
Sergey Kiba <serhiikiba@gmail.com>
2023-12-04 16:01:02 +0100

    add onClick Categories

--- pizza-shop/src/components/Categories.jsx
+++ pizza-shop/src/components/Categories.jsx
@@ -1,18 +1,27 @@
-
+import React from "react";
 
 function Categories() {
-    return (
-      <div class="categories">
-        <ul>
-          <li class="active">Все</li>
-          <li>Мясные</li>
-          <li>Вегетарианская</li>
-          <li>Гриль</li>
-          <li>Острые</li>
-          <li>Закрытые</li>
-        </ul>
-      </div>
-    );
-  }
+    const [activeIndex, setActiveIndex] = React.useState(0);
 
-  export default Categories;
+    const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
+
+    const handleClick = (index) => {
+        setActiveIndex(index);
+    };
+    return (
+        <div className="categories">
+            <ul>
+                {categories.map(category => (
+                    <li
+                        key={category}
+                        className={activeIndex === categories.indexOf(category)? 'active' : ''}
+                        onClick={() => handleClick(categories.indexOf(category))}>
+                        {category}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default Categories;
