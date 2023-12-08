724453ebe8ed9909ca463682a992411821a37a7d
Sergey Kiba <serhiikiba@gmail.com>
2023-12-08 19:18:20 +0100

    Added skeleton pizzas

--- pizza-shop/src/App.js
+++ pizza-shop/src/App.js
@@ -1,35 +1,20 @@
 import React from 'react';
 
 import Header from './components/Header';
-import Categories from './components/Categories';
-import Sort from './components/Sort';
-import PizzaBlock from './components/PizzaBlock';
-import pizzas from './assets/pizzas.json';
+import Home from "./pages/Home";
+import Notfound from "./pages/NotFound";
 
 import './scss/app.scss';
 
 function App() {
-    const [items, setItems] = React.useState([]);
 
-    React.useEffect(() => {
-        fetch('https://6572056dd61ba6fcc0143cae.mockapi.io/items')
-            .then(res => res.json())
-            .then(data => setItems(data));
-    }, []);
 
     return (
         <div className="wrapper">
             <Header/>
             <div className="content">
                 <div className="container">
-                    <div className="content__top">
-                        <Categories/>
-                        <Sort/>
-                    </div>
-                    <h2 className="content__title">Все пиццы</h2>
-                    <div className="content__items">
-                        {items.map(pizza => <PizzaBlock key={pizza.id} {...pizza}/>)}
-                    </div>
+                    <Home/>
                 </div>
             </div>
         </div>
